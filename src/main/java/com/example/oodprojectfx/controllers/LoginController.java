7332c9e73b51de084e35c89dfb724f1e022afc2e
DinurakshanRavichandran <dinurakshan@gmail.com>
2024-11-01 15:12:12 +0530

    This commit corrects a lot of UI errors and fixes them

--- src/main/java/com/example/oodprojectfx/controllers/LoginController.java
+++ src/main/java/com/example/oodprojectfx/controllers/LoginController.java
@@ -1,4 +1,33 @@
 package com.example.oodprojectfx.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    @FXML
+    private PasswordField PasswordField;
+
+    @FXML
+    private TextField UsernameField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    void onLoginButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSignUpButtonClick(ActionEvent event) {
+
+    }
+
 }
