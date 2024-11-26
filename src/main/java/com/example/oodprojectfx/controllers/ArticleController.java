67a854338c792a042369b84a9590140fba074ea4
DinurakshanRavichandran <dinurakshan@gmail.com>
2024-11-27 01:20:16 +0530

    This commit adds the logic to the like button and it is yet to be tested - commit 10

--- src/main/java/com/example/oodprojectfx/controllers/ArticleController.java
+++ src/main/java/com/example/oodprojectfx/controllers/ArticleController.java
@@ -1,4 +1,85 @@
 package com.example.oodprojectfx.controllers;
 
+import com.example.oodprojectfx.database.DatabaseHandler;
+import com.example.oodprojectfx.models.Article;
+import com.example.oodprojectfx.models.UserSession;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ArticleController {
+
+    @FXML
+    public TextField titleField;
+    @FXML
+    public TextArea articleTextField;
+    @FXML
+    public Button likeButton;
+    private Article selectedArticle;
+    UserSession userSession;
+
+    @FXML
+    public void onBackButtonClick(ActionEvent event) {
+
+    }
+    @FXML
+    public void onLikeButtonClick(ActionEvent event) {
+        userSession = UserSession.getInstance();
+        String userEmail = userSession.getCurrentUser().getEmail();
+        String articleId =  selectedArticle.getArticleId(); // in the database the article id is an int
+
+        try{
+            //check if the article is already liked
+            String queryCheck = "SELECT liked_article FROM user_article_alloc WHERE email = '" + userEmail + "' AND article_id = " + articleId;
+            ResultSet resultSet = DatabaseHandler.search(queryCheck);
+
+            boolean isLiked = false;
+            if(resultSet.next()) {
+                isLiked = resultSet.getBoolean("liked_article");
+
+            }if (!isLiked)
+            {
+                //If not liked like the article
+                String queryLikeArticle = "UPDATE article SET no_of_likes = no_of_likes + 1 WHERE article_id = " + articleId;
+                DatabaseHandler.iud(queryLikeArticle);
+
+                String queryLikeAlloc = "UPDATE user_article_alloc SET liked_article = true WHERE email = '" + userEmail + "' AND article_id = " + articleId;
+                DatabaseHandler.iud(queryLikeAlloc);
+
+                likeButton.setText("Liked");
+
+
+            }else{
+                //If the article is already liked unlike the article
+                String queryUnlikeArticle = "UPDATE article SET no_of_likes = no_of_likes - 1 WHERE article_id = " + articleId;
+                DatabaseHandler.iud(queryUnlikeArticle);
+
+                String queryUnlikeAlloc = "UPDATE user_article_alloc SET liked_article = false WHERE email = '" + userEmail + "' AND article_id = " + articleId;
+                DatabaseHandler.iud(queryUnlikeAlloc);
+
+                likeButton.setText("like");
+            }
+        }catch (SQLException e)
+        {
+            System.out.println("Error handling like unlike action");
+            e.printStackTrace();
+
+        }
+
+    }
+
+    @FXML
+    public void setArticle(Article article)
+    {
+        //Populate the fields with the article's details
+        titleField.setText(article.getTitle());
+        articleTextField.setText(article.getArticleText());
+        this.selectedArticle = article;
+    }
+
 }
--- src/main/java/com/example/oodprojectfx/controllers/UserArticleViewViewController.java
+++ src/main/java/com/example/oodprojectfx/controllers/UserArticleViewViewController.java
@@ -3,10 +3,16 @@ package com.example.oodprojectfx.controllers;
 import com.example.oodprojectfx.models.Article;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.List;
 
 public class UserArticleViewViewController {
@@ -26,11 +32,13 @@ public class UserArticleViewViewController {
 
     private List<Article> articleList; // This is to store the articles passed from the article page user view
     private Article selectedArticle; // Passed for storing the article object
+    private int currentIndex;
 
     @FXML
     public void setArticle(Article article)
     {
         this.selectedArticle = article;
+
 
         //Populate fields with article details
         titleField.setText(article.getTitle());
@@ -45,6 +53,19 @@ public class UserArticleViewViewController {
     public void setArticleList(List<Article> articleList )
     {
         this.articleList = articleList;
+        this.currentIndex = 0; // start at the first index
+
+    }
+
+    private void loadArticleAtIndex(int index)
+    {
+        if(index >= 0  && index < articleList.size())
+        {
+            Article article = articleList.get(index);
+            setArticle(article);
+        }else{
+            System.out.println("No more articles to display ");
+        }
     }
 
     @FXML
@@ -53,9 +74,32 @@ public class UserArticleViewViewController {
 
     @FXML
     public void onSkipButtonClick(ActionEvent event) {
+        if(currentIndex < articleList.size() - 1)
+        {
+            currentIndex ++;
+        }else {
+            currentIndex = 0;
+        }
+        // Update selectedArticle to the current article
+        setArticle(articleList.get(currentIndex));
     }
 
     @FXML
     public void onReadButtonClick(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/oodprojectfx/views/articlePage-view.fxml"));
+            Parent root = loader.load();
+
+            // Pass the article details to the ArticleDetailsController
+            ArticleController controller = loader.getController();
+            controller.setArticle(selectedArticle);// sending the article from this class to the next
+
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
